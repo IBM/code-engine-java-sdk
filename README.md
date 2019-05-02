a3b7e5d05fcc39b0c0f1d4228282f55de33b91ee
Phil Adams <phil_adams@us.ibm.com>
2019-05-02 16:46:13 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# java-sdk-template
-Template for IBM Cloud Java SDK
+# IBM Cloud Java SDK Template
+This repository serves as a template for Java SDKs that are produced with the 
+[IBM OpenAPI SDK Generator](https://github.ibm.com/CloudEngineering/openapi-sdkgen).
+
+You can use the contents of this repository to create your own Java SDKs.
+
+## How to use this repository
+
+##### 1. Copy the repository
+Copy the files contained in this repository as a starting point when building your own Java SDK
+for one or more IBM Cloud services.
+
+##### 2. Modify the copied files to reflect your SDK
+The following files will need to be modified after copying them from this template repository:
+* pom.xml - comments inside the file will guide you when customizing this for your own SDK
+
+
+* SdkCommon.java - Java SDKs built with the IBM OpenAPI SDK Generator 
+need to provide a class called `SDKCommon` that is invoked by the generated service methods.
+The class provided in this template repository has a package name of `com.ibm.cloud.mysdk.common`. This package
+will need to be adjusted to reflect your own SDK's base package name.  For example, if you configure your 
+API definition to specify a Java base package name of `com.ibm.mygroup.mysdk`, you'll need to rename the directories
+underneath `src/main/java` and modify the SdkCommon class itself to reflect that package name.
+Inside this class, you'll find a method called `getSdkHeaders()`.  This method should be modified so that it returns
+the desired set of SDK-specific HTTP headers to be included in outgoing REST API requests.  The example class returns the
+`User-Agent` HTTP header, but you can modify this to return any set of HTTP headers that meet your SDK requirements,
+including none.
+
+##### 3. Generate the Java code with the IBM OpenAPI SDK Generator
+This is the step that you've been waiting for!
+
+In this step, you'll invoke the IBM OpenAPI SDK Generator to process your API definition.
+
+This will generate a collection of Java classes which will be included in your SDK project.
+You'll find instructions on how to do this [here](https://github.ibm.com/CloudEngineering/openapi-sdkgen/wiki).
---
+++ src/main/java/com/ibm/cloud/mysdk/common/SdkCommon.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ibm.cloud.mysdk.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ibm.cloud.sdk.core.http.HttpHeaders;
+import com.ibm.cloud.sdk.core.util.RequestUtils;
+
+/**
+ * The purpose of this class is to provide SDK-specific HTTP headers to be included in REST API requests made
+ * by the SDK.
+ */
+public class SdkCommon {
+
+    /**
+     * SDK-specific "user agent" value.
+     */
+    private static String userAgent;
+
+    private SdkCommon() {
+    }
+
+    private static String loadSdkVersion() {
+        return "1.0.0";
+    }
+
+    /**
+     * This function uses lazy initialization to build the SDK's "user agent" value.
+     * This will consist of the SDK name and version, followed by
+     * a set of properties provided by the Java Core's RequestUtils class.
+     * @return the SDK-specific user agent value
+     */
+    private static String getUserAgent() {
+        if (userAgent == null) {
+            userAgent = "MySDK/" + loadSdkVersion() + "; " + RequestUtils.getUserAgent();
+        }
+        return userAgent;
+    }
+
+    /**
+     * This function is invoked by generated service methods (i.e. methods which implement the REST API operations
+     * defined within the API definition). The purpose of this function is to give the SDK implementor the opportunity
+     * to provide SDK-specific HTTP headers that will be sent with an outgoing REST API request.
+     * <p>This function is invoked for <b>each</b> invocation of a generated service method,
+     * so the set of HTTP headers could be request-specific.
+     * As an optimization, if your SDK will be returning the same set of HTTP headers for each invocation of this
+     * function, it is recommended that you initialize the returned map just once (perhaps by using
+     * lazy initialization) and simply return it each time the function is invoked, instead of building it each time
+     * as in the example below.
+     *
+     * @param serviceName
+     *            the name of the service as defined in the API definition (e.g. "MyService1")
+     * @param serviceVersion
+     *            the version of the service as defined in the API definition (e.g. "V1")
+     * @param operationId
+     *            the operationId as defined in the API definition (e.g. getContext)
+     * @return a Map which contains
+     */
+    public static Map<String, String> getSdkHeaders(String serviceName, String serviceVersion, String operationId) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(HttpHeaders.USER_AGENT, getUserAgent());
+        return headers;
+    }
+}
