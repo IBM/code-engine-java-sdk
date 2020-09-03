17b947558e12a125305d83d8ce10be65f2832fa8
Eric Millin <eric.t.millin@ibm.com>
2020-09-03 12:17:32 -0400

    wip

---
+++ modules/ibm-cloud-code-engine/src/main/java/com/ibm/cloud/code_engine/ibm_cloud_code_engine/v1/IbmCloudCodeEngine.java
@@ -0,0 +1,105 @@
+/*
+ * (C) Copyright IBM Corp. 2020.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/*
+ * IBM OpenAPI SDK Code Generator Version: 3.12.0-64fe8d3f-20200820-144050
+ */
+
+package com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1;
+
+import com.ibm.cloud.code_engine.common.SdkCommon;
+import com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1.model.ListKubeconfigOptions;
+import com.ibm.cloud.sdk.core.http.RequestBuilder;
+import com.ibm.cloud.sdk.core.http.ResponseConverter;
+import com.ibm.cloud.sdk.core.http.ServiceCall;
+import com.ibm.cloud.sdk.core.security.Authenticator;
+import com.ibm.cloud.sdk.core.security.ConfigBasedAuthenticatorFactory;
+import com.ibm.cloud.sdk.core.service.BaseService;
+import com.ibm.cloud.sdk.core.util.ResponseConverterUtils;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * The purpose is to provide an API to get Kubeconfig for IBM Cloud Code Engine Project.
+ *
+ * @version v1
+ */
+public class IbmCloudCodeEngine extends BaseService {
+
+  public static final String DEFAULT_SERVICE_NAME = "ibm_cloud_code_engine";
+
+  public static final String DEFAULT_SERVICE_URL = "https://api.us-south.codeengine.cloud.ibm.com/api/v1";
+
+ /**
+   * Class method which constructs an instance of the `IbmCloudCodeEngine` client.
+   * The default service name is used to configure the client instance.
+   *
+   * @return an instance of the `IbmCloudCodeEngine` client using external configuration
+   */
+  public static IbmCloudCodeEngine newInstance() {
+    return newInstance(DEFAULT_SERVICE_NAME);
+  }
+
+  /**
+   * Class method which constructs an instance of the `IbmCloudCodeEngine` client.
+   * The specified service name is used to configure the client instance.
+   *
+   * @param serviceName the service name to be used when configuring the client instance
+   * @return an instance of the `IbmCloudCodeEngine` client using external configuration
+   */
+  public static IbmCloudCodeEngine newInstance(String serviceName) {
+    Authenticator authenticator = ConfigBasedAuthenticatorFactory.getAuthenticator(serviceName);
+    IbmCloudCodeEngine service = new IbmCloudCodeEngine(serviceName, authenticator);
+    service.configureService(serviceName);
+    return service;
+  }
+
+  /**
+   * Constructs an instance of the `IbmCloudCodeEngine` client.
+   * The specified service name and authenticator are used to configure the client instance.
+   *
+   * @param serviceName the service name to be used when configuring the client instance
+   * @param authenticator the {@link Authenticator} instance to be configured for this client
+   */
+  public IbmCloudCodeEngine(String serviceName, Authenticator authenticator) {
+    super(serviceName, authenticator);
+    setServiceUrl(DEFAULT_SERVICE_URL);
+  }
+
+  /**
+   * Retrieve KUBECONFIG for a specified project.
+   *
+   * Returns the KUBECONFIG, similar to the output of `kubectl config view --minify=true`.
+   *
+   * @param listKubeconfigOptions the {@link ListKubeconfigOptions} containing the options for the call
+   * @return a {@link ServiceCall} with a void result
+   */
+  public ServiceCall<Void> listKubeconfig(ListKubeconfigOptions listKubeconfigOptions) {
+    com.ibm.cloud.sdk.core.util.Validator.notNull(listKubeconfigOptions,
+      "listKubeconfigOptions cannot be null");
+    String[] pathSegments = { "namespaces", "config" };
+    String[] pathParameters = { listKubeconfigOptions.id() };
+    RequestBuilder builder = RequestBuilder.get(RequestBuilder.constructHttpUrl(getServiceUrl(), pathSegments, pathParameters));
+    Map<String, String> sdkHeaders = SdkCommon.getSdkHeaders("ibm_cloud_code_engine", "v1", "listKubeconfig");
+    for (Entry<String, String> header : sdkHeaders.entrySet()) {
+      builder.header(header.getKey(), header.getValue());
+    }
+    builder.header("Refresh-Token", listKubeconfigOptions.refreshToken());
+    if (listKubeconfigOptions.accept() != null) {
+      builder.header("Accept", listKubeconfigOptions.accept());
+    }
+    ResponseConverter<Void> responseConverter = ResponseConverterUtils.getVoid();
+    return createServiceCall(builder.build(), responseConverter);
+  }
+
+}
---
+++ modules/ibm-cloud-code-engine/src/test/java/com/ibm/cloud/code_engine/ibm_cloud_code_engine/v1/IbmCloudCodeEngineTest.java
@@ -0,0 +1,158 @@
+/*
+ * (C) Copyright IBM Corp. 2020.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1;
+
+import com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1.IbmCloudCodeEngine;
+import com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1.model.ListKubeconfigOptions;
+import com.ibm.cloud.code_engine.ibm_cloud_code_engine.v1.utils.TestUtilities;
+import com.ibm.cloud.sdk.core.http.Response;
+import com.ibm.cloud.sdk.core.security.Authenticator;
+import com.ibm.cloud.sdk.core.security.NoAuthAuthenticator;
+import com.ibm.cloud.sdk.core.service.model.FileWithMetadata;
+
+import com.ibm.cloud.sdk.core.util.EnvironmentUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * Unit test class for the IbmCloudCodeEngine service.
+ */
+@PrepareForTest({ EnvironmentUtils.class })
+@PowerMockIgnore({"javax.net.ssl.*", "org.mockito.*"})
+public class IbmCloudCodeEngineTest extends PowerMockTestCase {
+
+  final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
+  final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
+
+  protected MockWebServer server;
+  protected IbmCloudCodeEngine ibmCloudCodeEngineService;
+
+  // Creates a mock set of environment variables that are returned by EnvironmentUtils.getenv().
+  private Map<String, String> getTestProcessEnvironment() {
+    Map<String, String> env = new HashMap<>();
+    env.put("TESTSERVICE_AUTH_TYPE", "noAuth");
+    return env;
+  }
+
+  public void constructClientService() throws Throwable {
+    PowerMockito.spy(EnvironmentUtils.class);
+    PowerMockito.when(EnvironmentUtils.getenv()).thenReturn(getTestProcessEnvironment());
+    final String serviceName = "testService";
+
+    ibmCloudCodeEngineService = IbmCloudCodeEngine.newInstance(serviceName);
+    String url = server.url("/").toString();
+    ibmCloudCodeEngineService.setServiceUrl(url);
+  }
+
+  /**
+  * Negative Test - construct the service with a null authenticator.
+  */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testConstructorWithNullAuthenticator() throws Throwable {
+    final String serviceName = "testService";
+
+    new IbmCloudCodeEngine(serviceName, null);
+  }
+
+  @Test
+  public void testListKubeconfigWOptions() throws Throwable {
+    // Schedule some responses.
+    String mockResponseBody = "";
+    String listKubeconfigPath = java.net.URLEncoder.encode("/namespaces/testString/config", "UTF-8").replace("%2F", "/");
+
+    server.enqueue(new MockResponse()
+    .setResponseCode(200)
+    .setBody(mockResponseBody));
+
+    constructClientService();
+
+    // Construct an instance of the ListKubeconfigOptions model
+    ListKubeconfigOptions listKubeconfigOptionsModel = new ListKubeconfigOptions.Builder()
+    .refreshToken("testString")
+    .id("testString")
+    .accept("application/json")
+    .build();
+
+    // Invoke operation with valid options model (positive test)
+    Response<Void> response = ibmCloudCodeEngineService.listKubeconfig(listKubeconfigOptionsModel).execute();
+    assertNotNull(response);
+    Void responseObj = response.getResult();
+    // Response does not have a return type. Check that the result is null.
+    assertNull(responseObj);
+
+    // Verify the contents of the request
+    RecordedRequest request = server.takeRequest();
+    assertNotNull(request);
+    assertEquals(request.getMethod(), "GET");
+    assertEquals(request.getHeader("Refresh-Token"), "testString");
+
+    // Check query
+    Map<String, String> query = TestUtilities.parseQueryString(request);
+    assertNull(query);
+
+    // Check request path
+    String parsedPath = TestUtilities.parseReqPath(request);
+    assertEquals(parsedPath, listKubeconfigPath);
+  }
+
+  // Test the listKubeconfig operation with null options model parameter
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testListKubeconfigNoOptions() throws Throwable {
+    // construct the service
+    constructClientService();
+
+    server.enqueue(new MockResponse());
+
+    // Invoke operation with null options model (negative test)
+    ibmCloudCodeEngineService.listKubeconfig(null).execute();
+  }
+
+  /** Initialize the server */
+  @BeforeMethod
+  public void setUpMockServer() {
+    try {
+        server = new MockWebServer();
+        // register handler
+        server.start();
+        }
+    catch (IOException err) {
+        fail("Failed to instantiate mock web server");
+    }
+  }
+
+  @AfterMethod
+  public void tearDownMockServer() throws IOException {
+    server.shutdown();
+    ibmCloudCodeEngineService = null;
+  }
+}
